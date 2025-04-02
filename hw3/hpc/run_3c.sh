77ebfaae63540b12deeefab9e02fa339bbd20d08
Mina <ahaminasha@gmail.com>
2025-04-02 00:17:56 -0400

    change prompt for 3c

--- hw3/hpc/run_3c.sh
+++ hw3/hpc/run_3c.sh
@@ -1,5 +1,5 @@
 # Add all your Python calls here:
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Think critically and carefully,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Think critically and carefully and avoid bias,"
 python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Respond as an expert in that field,"
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Avoid making up information,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "A wrong answer might cause disaster,"
 # ... etc.
