eeacd3356165b71463eb325339b7ba8207eb6043
Mina <ahaminasha@gmail.com>
2025-04-01 23:41:09 -0400

    changed 3c prompt

--- hw3/hpc/run_3c.sh
+++ hw3/hpc/run_3c.sh
@@ -1,5 +1,5 @@
 # Add all your Python calls here:
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Avoid making up information; only respond based on what is verifiably correct,"
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Respond as an expert who only shares information backed by scientific evidence,"
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Double-check your knowledge and analyze reasoning step by step,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "According to evidence,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Respond as an expert in that field,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "After double-checked,"
 # ... etc.
