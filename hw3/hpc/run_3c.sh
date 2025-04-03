cf4cffc9c8a4c13d650c5c14188c8c893c5284c1
Mina <ahaminasha@gmail.com>
2025-04-03 16:20:50 -0400

    changed demo and prompt

--- hw3/hpc/run_3c.sh
+++ hw3/hpc/run_3c.sh
@@ -1,8 +1,9 @@
 # Add all your Python calls here:
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Contrary to popular belief and upon closer inspection,"
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Factually,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Contrary to popular belief and think carefully,"
 python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "In reality,"
+python truthfulqa.py facebook/opt-1.3b --demos demo2.txt --system-prompt "Contrary to popular belief,"
+python truthfulqa.py facebook/opt-1.3b --demos demo2.txt --system-prompt "Contrary to popular belief and think carefully,"
+python truthfulqa.py facebook/opt-1.3b --demos demo2.txt --system-prompt "In reality,"
 # python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Experts agree that,"
 # python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Upon closer inspection,"
-python truthfulqa.py facebook/opt-125m --demos demonstrations.txt --system-prompt "Contrary to popular belief and upon closer inspection,"
 # ... etc.
