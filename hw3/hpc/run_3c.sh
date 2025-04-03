901b8b3a04f8caec8798b3eb800f5b840741aded
Mina <ahaminasha@gmail.com>
2025-04-03 14:14:21 -0400

    updated run_3c prompt

--- hw3/hpc/run_3c.sh
+++ hw3/hpc/run_3c.sh
@@ -1,5 +1,8 @@
 # Add all your Python calls here:
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Think critically and carefully and avoid bias,"
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Respond as an expert in that field,"
-python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "A wrong answer might cause disaster,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Contrary to popular belief,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Careful reasoning suggests,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "To be precise,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Experts agree that,"
+python truthfulqa.py facebook/opt-1.3b --demos demonstrations.txt --system-prompt "Upon closer inspection,"
+python truthfulqa.py facebook/opt-125m --demos demonstrations.txt --system-prompt "Contrary to popular belief,"
 # ... etc.
